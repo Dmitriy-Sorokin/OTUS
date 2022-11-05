26b37001ff86935a2da8c9450bb56a916d539f8a
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-11-05 20:08:25 +0300

    assert

--- api_test_openbrewerydb/tests/test_brewery_api.py
+++ api_test_openbrewerydb/tests/test_brewery_api.py
@@ -16,4 +16,42 @@ def test_single_brewery():
     Assert.assert_value_field(get_result, "name", "MadTree Brewing")
     Assert.assert_value_field(get_result, "brewery_type", "regional")
     Assert.assert_value_field(get_result, "street", "3301 Madison Rd")
+    Assert.assert_value_field(get_result, "city", "Cincinnati")
+    Assert.assert_value_field(get_result, "state", "Ohio")
+    Assert.assert_value_field(get_result, "postal_code", "45209-1132")
+    Assert.assert_value_field(get_result, "country", "United States")
+    Assert.assert_value_field(get_result, "longitude", "-84.4239715")
+    Assert.assert_value_field(get_result, "latitude", "39.1563725")
+    Assert.assert_value_field(get_result, "phone", "5138368733")
+    Assert.assert_value_field(get_result, "website_url", "http://www.madtreebrewing.com")
+    Assert.assert_value_field(get_result, "updated_at", "2022-10-30T06:11:39.514Z")
+    Assert.assert_value_field(get_result, "created_at", "2022-10-30T06:11:39.514Z")
+
+
+def test_lest_breweries():
+    print("\nCheck dog_list status code, fields, fields value")
+    get_result: Response = Brewery.list_breweries()
+    one_object_resp = get_result.json()[0]
+    Assert.assert_status_code(get_result, 200)
+    Assert.assert_list_breweries_cerberus(get_result)
+    Assert.range_array_3_page(get_result)
+    Assert.assert_value_field(one_object_resp, "id", "10-56-brewing-company-knox")
+    Assert.assert_value_field(one_object_resp, "name", "10-56 Brewing Company")
+    Assert.assert_value_field(one_object_resp, "brewery_type", "micro")
+    Assert.assert_value_field(one_object_resp, "street", "400 Brown Cir")
+    Assert.assert_value_field(one_object_resp, "city", "Knox")
+    Assert.assert_value_field(one_object_resp, "state", "Indiana")
+    Assert.assert_value_field(one_object_resp, "postal_code", "46534")
+    Assert.assert_value_field(one_object_resp, "country", "United States")
+    Assert.assert_value_field(one_object_resp, "longitude", "-86.627954")
+    Assert.assert_value_field(one_object_resp, "latitude", "41.289715")
+    Assert.assert_value_field(one_object_resp, "phone", "6308165790")
+    Assert.assert_value_field(one_object_resp, "updated_at", "2022-10-30T06:11:39.514Z")
+    Assert.assert_value_field(one_object_resp, "created_at", "2022-10-30T06:11:39.514Z")
+
+def test_by_city():
+    print("\nCheck dog_list status code, fields, fields value")
+    get_result: Response = Brewery.filter_breweries_by_city()
+    Assert.assert_status_code(get_result, 200)
+    Assert.json_schema_by_city(get_result)
 
--- api_test_openbrewerydb/utils/assertions.py
+++ api_test_openbrewerydb/utils/assertions.py
@@ -18,7 +18,6 @@ class Assert:
         else:
             print(f"Wrong status code: " + str({response.status_code}))
 
-
     '''Method for checking presence of required fields in response to request'''
 
     @staticmethod
@@ -31,34 +30,91 @@ class Assert:
 
     @staticmethod
     def assert_value_field(response: Response, field_name, expected_value):
-        checked = response.json()
-        checked_field_value = checked.get(field_name)
+        checked_js = response
+        checked_field_value = checked_js.get(field_name)
+        print("Value field getting")
         assert checked_field_value == expected_value
-        print(expected_value + ' TRUE !!!')
+        print(expected_value + ' The value is correct!!!')
 
         '''Checked field by using cerberus'''
+
     @staticmethod
-    def assert_list_breweries(response: Response):
+    def assert_list_breweries_cerberus(response: Response):
+        resp = response.json()[0]
+        print(resp['id'])
         schema = {
-            'id': {"type": "list"},
+            'id': {"type": "string"},
             'name': {"type": "string"},
             'brewery_type': {"type": "string"},
             'street': {"type": "string"},
-            'address_2': {"type": "string"},
-            'address_3': {"type": "string"},
+            'address_2': {"type": "string", 'nullable': True},
+            'address_3': {"type": "string", 'nullable': True},
             'city': {"type": "string"},
             'state': {"type": "string"},
-            'county_province': {"type": "string"},
+            'county_province': {"type": "string", 'nullable': True},
             'postal_code': {"type": "string"},
             'country': {"type": "string"},
             'longitude': {"type": "string"},
-            'phone': {"type": "string"},
-            'website_url': {"type": "string"},
-            'updated_at': {"type": "string"},
-            'created_at': {"type": "string"},
             'latitude': {"type": "string"},
+            'phone': {"type": "string"},
+            'website_url': {"type": "string", 'nullable': True},
+            'updated_at': {"type": "string"},
+            'created_at': {"type": "string"}
+        }
+        v = cerberus.Validator()
+        assert v.validate(response.json()[0], schema)
+
+    @staticmethod
+    def range_array_3_page(response: Response):
+        checked = response.json()
+        print(len(checked))
+        assert len(checked) == 3
+
+    @staticmethod
+    def json_schema_by_city(response: Response):
+        """проверка структуры ответа на запрос"""
+        resp = response.json()[0]
+        schema = {
+            "type": "object",
+            "properties": {
+                'id': {"type": "string"},
+                'name': {"type": "string"},
+                'brewery_type': {"type": "string"},
+                'street': {"type": "string"},
+                'address_2': {"type": "null"},
+                'address_3': {"type": "null"},
+                'city': {"type": "string"},
+                'state': {"type": "string"},
+                'county_province': {"type": "null"},
+                'postal_code': {"type": "string"},
+                'country': {"type": "string"},
+                'longitude': {"type": "string"},
+                'latitude': {"type": "string"},
+                'phone': {"type": "string"},
+                'website_url': {"type": "string"},
+                'updated_at': {"type": "string"},
+                'created_at': {"type": "string"}
+            },
+            "required": ["id", "name", "brewery_type", "street", "city", "state", "postal_code",
+                         "country", "longitude", "latitude", "phone", "website_url", "updated_at", "created_at",
+                         "address_2", "address_3", "county_province"]
         }
 
-        v = cerberus.Validator()
-        assert v.validate(response.json(), schema)
+        validate(instance=response.json()[0], schema=schema)
+
+        assert resp["id"] == "10-barrel-brewing-co-san-diego"
+        assert resp["name"] == "10 Barrel Brewing Co"
+        assert resp["street"] == "1501 E St"
+        assert resp["city"] == "San Diego"
+        assert resp["state"] == "California"
+        assert resp["postal_code"] == "92101-6618"
+        assert resp["country"] == "United States"
+        assert resp["longitude"] == "-117.129593"
+        assert resp["latitude"] == "32.714813"
+        assert resp["phone"] == "6195782311"
+        assert resp["website_url"] == "http://10barrel.com"
+        assert resp["updated_at"] == "2022-10-30T06:11:39.514Z"
+        assert resp["created_at"] == "2022-10-30T06:11:39.514Z"
+
+
 
