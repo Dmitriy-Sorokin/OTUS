eec1c462463dec869683ade7fd77a851da4808c2
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-11-19 17:33:46 +0300

    silenium 4 start pageogject

--- conftest.py
+++ conftest.py
@@ -1,49 +1,88 @@
 import pytest
+import os
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.firefox.service import Service as FFService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver import Chrome, Firefox, Remote
 
+'''Lesson9_Selenium'''
+# def pytest_addoption(parser):
+#     parser.addoption("--maximized", action="store_true", help="Maximize browser windows")
+#     parser.addoption("--headless", action="store_true", help="Run headless")
+#     parser.addoption("--browser", action="store", default="chrome", choices=["chrome", "firefox"])
+#     parser.addoption("--url", action="store", default="https://demo.opencart.com/")
+#
+#
+# @pytest.fixture(scope="session")
+# def url(request):
+#     return request.config.getoption("--url")
+#
+#
+# @pytest.fixture
+# def browser(request):
+#     _browser = request.config.getoption("--browser")
+#     headless = request.config.getoption("--headless")
+#     maximized = request.config.getoption("--maximized")
+#
+#     driver = None
+#
+#     if _browser == "chrome":
+#         options = webdriver.ChromeOptions()
+#         if headless:
+#             options.headless = True
+#         # driver = webdriver.Chrome(executable_path="c:\soft\drivers\chromedriver", driver.maximize_window())
+#         driver = Chrome(executable_path=ChromeDriverManager().install(), options=options)
+#     elif _browser == "firefox":
+#         options = webdriver.FirefoxOptions()
+#         if headless:
+#             options.headless = True
+#         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+#         #driver = webdriver.Firefox(executable_path="c:\soft\drivers\geckodriver")
+#
+#     if maximized:
+#         driver.maximize_window()
+#
+#     def final():
+#         driver.quit()
+#
+#     request.addfinalizer(final)
+#
+#     return driver
+
+'''lesson10_element_search'''
+
 
 def pytest_addoption(parser):
-    parser.addoption("--maximized", action="store_true", help="Maximize browser windows")
-    parser.addoption("--headless", action="store_true", help="Run headless")
-    parser.addoption("--browser", action="store", default="chrome", choices=["chrome", "firefox"])
+    parser.addoption("--browser", action="store", default="chrome")
     parser.addoption("--url", action="store", default="https://demo.opencart.com/")
-
-
-@pytest.fixture(scope="session")
-def url(request):
-    return request.config.getoption("--url")
+    parser.addoption("--drivers", action="store", default=os.path.expanduser("c:\soft\drivers"))
 
 
 @pytest.fixture
 def browser(request):
+    # Сбор параметров для запуска pytest
     _browser = request.config.getoption("--browser")
-    headless = request.config.getoption("--headless")
-    maximized = request.config.getoption("--maximized")
-
-    driver = None
+    url = request.config.getoption("--url")
+    drivers = request.config.getoption("--drivers")
 
     if _browser == "chrome":
-        options = webdriver.ChromeOptions()
-        if headless:
-            options.headless = True
-        # driver = webdriver.Chrome(executable_path="c:\soft\drivers\chromedriver", driver.maximize_window())
-        driver = Chrome(executable_path=ChromeDriverManager().install(), options=options)
-    elif _browser == "firefox":
-        options = webdriver.FirefoxOptions()
-        if headless:
-            options.headless = True
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
-        #driver = webdriver.Firefox(executable_path="c:\soft\drivers\geckodriver")
+        # В selenium 4 рекомендуют использование такого подхода
+        service = ChromiumService(executable_path=drivers + "\chromedriver")
+        #driver = Chrome(executable_path=ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service)
+    # elif _browser == "firefox":
+    #     service = FFService(executable_path=drivers + "\geckodriver")
+    #     #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    #     driver = webdriver.Firefox(service=service)
 
-    if maximized:
-        driver.maximize_window()
+    driver.maximize_window()
 
-    def final():
-        driver.quit()
+    request.addfinalizer(driver.close)
 
-    request.addfinalizer(final)
+    driver.get(url)
+    driver.url = url
 
     return driver
--- lesson9_SELENIUM/tests/test_first.py
+++ lesson9_SELENIUM/tests/test_first.py
@@ -5,4 +5,3 @@ def test_first(browser, url):
     browser.get(url)
     assert browser.title == "Your Store"
     time.sleep(5)
-
