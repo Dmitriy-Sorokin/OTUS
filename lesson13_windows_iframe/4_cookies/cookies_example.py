3d8917547fa0d9876853381f40d25b69774bffe6
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-12-02 17:55:38 +0300

    window iframe

---
+++ lesson13_windows_iframe/4_cookies/cookies_example.py
@@ -0,0 +1,16 @@
+import time
+
+from config import CHROMEDRIVER
+from selenium import webdriver
+
+chrome = webdriver.Chrome(CHROMEDRIVER)
+chrome.get("https://yandex.ru")
+
+# cookies = chrome.get_cookies()
+
+saved_cookie = chrome.get_cookie("mda")
+saved_cookie["value"] = "0873247234723472984723098174092381740923874"
+
+chrome.add_cookie(cookie_dict=saved_cookie)
+
+chrome.quit()
