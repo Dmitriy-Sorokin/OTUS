63501200c959878c2cbcc1980264f0f5bb28de92
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-12-12 22:31:39 +0300

    adminka

--- home_task_page_object/page_object/adminka.py
+++ home_task_page_object/page_object/adminka.py
@@ -11,6 +11,14 @@ class Admin(BasePage):
     HEADING_PANEL = (By.CSS_SELECTOR, "#content > div > div > div > div > div.panel-heading > h1")
     ERROR_TEXT = (By.XPATH, "/html/body/div/div/div/div/div/div/div[2]/div")
     CLOSE_ALERT = (By.CSS_SELECTOR, "#content > div > div > div > div > div.panel-body > div > button")
+    PANEL_FORGOT_PASSWORD = (By.CSS_SELECTOR, ".panel-title")
+    LABEL_EMAIL = (By.CSS_SELECTOR, "#content > div > div > div > div > div.panel-body > form > div.form-group > label")
+    INPUT_EMAIL = (By.ID, "#input-email")
+    RESET_BTN = (By.CSS_SELECTOR, ".btn-primary")
+    ALERT_WARNING = (By.CSS_SELECTOR, ".alert-danger")
+    CLOSE_ALERT_WARNING = (By.CSS_SELECTOR, ".close")
+    CANSEL_BTN = (By.CSS_SELECTOR, ".btn-default")
+    LINK_FORGOT = (By.LINK_TEXT, "Forgotten Password")
 
     def check_elements(self):
         time.sleep(2)
@@ -32,7 +40,6 @@ class Admin(BasePage):
         assert text == str("No match for Username and/or Password.")
         self._click(self.CLOSE_ALERT)
 
-
     def login_in_adminka(self, username, password):
         self.get_param_url("/admin/")
         self._element(self.INPUT_USERNAME).clear()
@@ -40,3 +47,15 @@ class Admin(BasePage):
         self._element(self.INPUT_PASSWORD).clear()
         self._element(self.INPUT_PASSWORD).send_keys(password)
         self._click(self.BTN_LOGIN)
+
+    def check_elem_at_page_forgot_password(self):
+        self.get_param_url("/admin/")
+        self._click(self.LINK_FORGOT)
+        self._verify_element_present(self.PANEL_FORGOT_PASSWORD)
+        self._verify_element_present(self.LABEL_EMAIL)
+        self._verify_element_present(self.INPUT_EMAIL)
+        self._verify_element_present(self.RESET_BTN)
+        self._verify_element_present(self.CANSEL_BTN)
+
+    def input_incorrect_data_for_email(self, email):
+        pass
