1434d6175535f06a94c382c93b546946093244e1
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-12-13 19:11:30 +0300

    reg user

--- home_task_page_object/page_object/register_user.py
+++ home_task_page_object/page_object/register_user.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from home_task_page_object.page_object.BasePage import BasePage
+from random_word import RandomWords
 import time
 
 
@@ -16,6 +17,11 @@ class Register(BasePage):
     P_POLICY = (By.CSS_SELECTOR, "#content > form > div > div > input[type=checkbox]:nth-child(2)")
     BTN_CONTINUE = (By.CSS_SELECTOR, "#content > form > div > div > input.btn.btn-primary")
     ALERT_DANGER = (By.CSS_SELECTOR, ".alert-danger")
+    RB_YES = (By.CSS_SELECTOR, ".radio-inline input")
+    RB_NO = (
+        By.CSS_SELECTOR, "#content > form > fieldset:nth-child(3) > div > div > label:nth-child(2) > input[type=radio]")
+    AC_CREATED = (By.CSS_SELECTOR, ".col-sm-9 h1")
+    CONTINUE = (By.LINK_TEXT, "Continue")
 
     def check_element_reg_user(self):
         self._verify_element_present(self.MY_ACCOUNT).click()
@@ -36,3 +42,30 @@ class Register(BasePage):
         expected_text = "Warning: You must agree to the Privacy Policy!"
         assert alert_text == expected_text
         time.sleep(1)
+
+    def checked_input_not_correctly_data(self):
+        random_words = RandomWords()
+        self._verify_element_present(self.MY_ACCOUNT).click()
+        time.sleep(1)
+        self._verify_element_present(self.REG).click()
+        time.sleep(1)
+        self._element(self.FIRST_NAME).clear()
+        self._element(self.FIRST_NAME).send_keys("first_name")
+        self._element(self.LAST_NAME).clear()
+        self._element(self.LAST_NAME).send_keys("last name")
+        self._element(self.E_MAIL).clear()
+        self._element(self.E_MAIL).send_keys(f"{random_words.get_random_word()}@email.com")
+        self._element(self.PHONE).clear()
+        self._element(self.PHONE).send_keys("1234455")
+        self._element(self.PASS_INP).clear()
+        self._element(self.PASS_INP).send_keys("zxcasd")
+        self._element(self.PASS_CONF).clear()
+        self._element(self.PASS_CONF).send_keys("zxcasd")
+        self._click(self.RB_YES)
+        self._click(self.RB_NO)
+        self._click(self.P_POLICY)
+        self._click(self.BTN_CONTINUE)
+        text = self.browser.find_element(*self.AC_CREATED).text
+        text_created = "Your Account Has Been Created!"
+        assert text == text_created
+        self._click(self.CONTINUE)
--- home_task_page_object/tests/test_adminka.py
+++ home_task_page_object/tests/test_adminka.py
@@ -4,11 +4,20 @@ from home_task_page_object.page_object.adminka import Admin
 
 
 def test_adminka(browser):
-    # Admin(browser).check_elements()
-    # time.sleep(1)
-    # Admin(browser).check_error("user", "bitnami1")
-    # time.sleep(1)
-    # Admin(browser).login_in_adminka("user", "bitnami")
+    """
+    Test page (admin):
+    Checking the presence of the necessary elements on the page
+    Login to the admin panel
+    Check email for validity
+    The time slips are for illustrative purposes
+    """
+    Admin(browser).check_elements()
+    time.sleep(1)
+    Admin(browser).check_error("user", "bitnami1")
+    time.sleep(1)
+    Admin(browser).login_in_adminka("user", "bitnami")
     time.sleep(1)
     Admin(browser).check_elem_at_page_forgot_password()
+    time.sleep(1)
+    Admin(browser).input_incorrect_data_for_email("asdfg@gmail.com")
     time.sleep(1)
