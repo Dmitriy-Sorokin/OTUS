54bc50991fc0750f072d3beba595f8fb5aba207a
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-12-15 18:34:06 +0300

    add card

--- home_task_page_object/tests/test_reg_user.py
+++ home_task_page_object/tests/test_reg_user.py
@@ -3,7 +3,7 @@ from home_task_page_object.page_object.register_user import Register
 
 
 def test_reg_user(browser):
-    # Register(browser).check_element_reg_user()
+    Register(browser).check_element_reg_user()
     time.sleep(1)
     Register(browser).checked_input_not_correctly_data()
     time.sleep(1)
