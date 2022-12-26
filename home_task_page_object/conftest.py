cc5c62c991de0a5342297d10eb0d1ac6e9d6e3db
Dmitriy Sorokin <sorokin.da92@gmail.com>
2022-12-26 18:22:54 +0300

    add allure

--- home_task_page_object/conftest.py
+++ home_task_page_object/conftest.py
@@ -9,8 +9,8 @@ def pytest_addoption(parser):
     parser.addoption("--maximized", action="store_true", help="Maximize browser windows")
     parser.addoption("--headless", action="store_true", help="Run headless")
     parser.addoption("--browser", action="store", default="chrome", choices=["chrome", "firefox"])
-    parser.addoption("--url", action="store", default="http://10.48.67.16:8081")
-    # parser.addoption("--url", action="store", default="http://192.168.0.112:8081")
+    # parser.addoption("--url", action="store", default="http://10.48.67.16:8081")
+    parser.addoption("--url", action="store", default="http://192.168.0.112:8081")
 
 
 @pytest.fixture
